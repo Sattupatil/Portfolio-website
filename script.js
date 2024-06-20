c50e2ebece0c801998f3c302c8a2e1dcdba24de1
Jadhav Satishkumar <119030673+Sattupatil@users.noreply.github.com>
2024-06-20 14:02:43 +0530

    first commit

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('#skills .skill').forEach(function (skill) {
-        skill.addEventListener('dblclick', function () {
-            alert('Detailed view of the skill: ' + skill.textContent);
-        });
-    });
-});
